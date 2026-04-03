b6abcf9b3d285136e7bcfd94648968d4522cf0eb
Deepavalli <dandudeepa27@gmail.com>
2026-04-03 11:31:40 +0530

    Create Visual SLAM.py

---
+++ Visual SLAM.py
@@ -0,0 +1,18 @@
+# SESSION 14 – Visual SLAM (Pose Update)
+
+# Step 1: Input previous pose
+prev_x = 2
+prev_y = 3
+
+# Step 2: Input motion
+dx = 0.4
+dy = 0.6
+
+# Step 3: Compute new pose
+new_x = prev_x + dx
+new_y = prev_y + dy
+
+# Step 4: Display result
+print("New Pose = (", new_x, ",", new_y, ")")
+
+print("\nProgram Executed Successfully")
